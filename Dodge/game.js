adc61ee9951c873322ee04152b34f7416c4d5e7c
Kaiwen Song <jirachiji@gmail.com>
2017-10-26 02:36:58 +0000

    change back to normal size

--- Dodge/game.js
+++ Dodge/game.js
@@ -13,7 +13,7 @@ function preload(){
 }
 function setup() {
     
-    createCanvas(1920,1080);
+    createCanvas(256,256);
     player = createSprite(width/2, height-(playerImage.height/2), 0, 0 );
     player.addImage(playerImage);
     enemy = createSprite (width/2, 0, 0, 0);
